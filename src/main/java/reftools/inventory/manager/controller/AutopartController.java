ed2465186bfeff5d300128a4b48ab96ca010bf9f
eliantte00 <syntaxys000@gmail.com>
2025-12-06 19:21:49 -0800

    RF1.3

--- src/main/java/reftools/inventory/manager/controller/AutopartController.java
+++ src/main/java/reftools/inventory/manager/controller/AutopartController.java
@@ -3,6 +3,7 @@ package reftools.inventory.manager.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,16 @@ public class AutopartController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            autopartService.deleteAutopart(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
